60506841c1daf52e58e23ffcbe6660b601924837
Daniel michael <michaellemakanit@gmail.com>
2026-06-24 15:44:27 +0300

    conditionals

---
+++ conditionals.js
@@ -0,0 +1,24 @@
+let x = Number(prompt("enter a random number: "))
+
+if (x % 2 == 0){
+    console.log("Even Numaber")
+
+}
+else{
+    console.log("Odd Number")
+
+}
+
+let num1 = Number(prompt("Entern num1"))
+let num2 = Number(prompt("Entern num2"))
+let num3 = Number(prompt("Entern num3"))
+
+if ((num1 > num2) && (num1 > num3)){
+    console.log("Num1 is the largest")
+}
+else if ( ("Num2 is the largest")){
+    console.log("Num2 is the largest")
+}
+else{
+    console.log("Num3 is the largest")
+}
