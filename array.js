fee5e571f2d111947c136fabc96824b0b4499e40
Daniel michael <michaellemakanit@gmail.com>
2026-06-24 16:17:53 +0300

    array

---
+++ array.js
@@ -0,0 +1,7 @@
+let arr = [1,2,3, "JS", "python", true, 34.78]
+console.log(arr)//(7) [1, 2, 3, 'JS', 'python', true, 34.78]
+
+console.log(arr[4])
+console.log(arr.at[-3])
+console.log(arr.at[3])
+
--- conditionals.js
+++ conditionals.js
@@ -1,24 +1,34 @@
-let x = Number(prompt("enter a random number: "))
+// // let x = Number(prompt("enter a random number: "))
 
-if (x % 2 == 0){
-    console.log("Even Numaber")
+// // if (x % 2 == 0){
+// //     console.log("Even Numaber")
 
-}
-else{
-    console.log("Odd Number")
+// // }
+// // else{
+// //     console.log("Odd Number")
 
-}
+// }
 
-let num1 = Number(prompt("Entern num1"))
-let num2 = Number(prompt("Entern num2"))
-let num3 = Number(prompt("Entern num3"))
+// let even = x % 2 == 0 ? "Even Number" : "Odd Number"
+// console.log(even)
 
-if ((num1 > num2) && (num1 > num3)){
-    console.log("Num1 is the largest")
-}
-else if ( ("Num2 is the largest")){
-    console.log("Num2 is the largest")
-}
-else{
-    console.log("Num3 is the largest")
-}
+let x = Number(prompt("Please enter your age:"));
+
+let citizen = x >= 18 ? "Eligible to vote" : "Not Eligible to Vote, you must be 18 and above";
+
+console.log(citizen);
+alert(citizen); 
+
+// let num1 = Number(prompt("Entern num1"))
+// let num2 = Number(prompt("Entern num2"))
+// let num3 = Number(prompt("Entern num3"))
+
+// if ((num1 > num2) && (num1 > num3)){
+//     console.log("Num1 is the largest")
+// }
+// else if ( ("Num2 is the largest")){
+//     console.log("Num2 is the largest")
+// }
+// else{
+//     console.log("Num3 is the largest")
+// }
